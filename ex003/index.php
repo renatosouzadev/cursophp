3113589917ef7a9c26e6e31f62bef7ec9ec58bc7
r3nat02207 <r3n4t0g4t0@gmail.com>
2023-04-09 19:01:48 -0300

    teste

---
+++ ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tipos primitivos do PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        //$num = 300;
+        //echo " <p>O valor da variavel é $num </p>";
+        $v = 300;
+        var_dump($v);
+    ?>
+</body>
+</html>
